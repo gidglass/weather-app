d8bc9aa47bb483132ef7fab84e086c8626e0ca79
Gideon Glass <gidglass@gmail.com>
2020-09-11 09:19:54 -0400

    Extracted first component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import icon from './icons/sun.png';
+import CurrentWeather from './components/CurrentWeather';
 
 function App() {
   // Sample data
@@ -117,21 +117,7 @@ function App() {
 
   return (
     <div className="weather-container">
-      <div className="current-weather">
-        <div className="current-weather__icon">
-          <img src={icon} href="#" alt="Todays weather icon" />
-        </div>
-        <div className="current-weather__description">
-          <h3 className="current-weather__description-location">Current Location</h3>
-          <p className="current-weather__description-condition">Clear</p>
-          <p className="current-weather__description-precip">Chance of Rain: 0%</p>
-        </div>
-        <div className="current-weather__temp">
-          <h3 className="current-weather__now">75°</h3>
-          <p className="current-weather__low">53°</p>
-          <p className="current-weather__high">81°</p>
-        </div>
-      </div>
+      <CurrentWeather />
       <div className="forecast">
         { daily.map(day => {
           return (
